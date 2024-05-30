d4694075230c8f636b6578daee39853982972cd2
Walkbn <Walk2233bueno1@gmail.com>
2024-05-29 22:14:29 -0300

    Update CalcularMediaNotas.java

--- CalcularMediaNotas.java
+++ CalcularMediaNotas.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 public class CalcularMediaNotas {
     public static void main(String[]args) {
         System.out.println("Digite as notas do aluno: ");
-        double nota[] = new double[4];
-        Scanner sc = new Scanner(System.in);
+        double nota[] = new double[4]; Scanner sc = new Scanner(System.in);
         for (int i = 0; i<4; i++) {
             System.out.print(i+1 + "° Nota do aluno: ");
             nota[i] = sc.nextInt();
         }
         double media = 0;
         for (int i =0; i<4; i++) { media= media+nota[i]; }
-        media = media/4;
-        sc.close();
+        media = media/4; sc.close();
         System.out.println("A média do aluno é: " + media);
+        if (media<5) { System.out.print("Reprovado");
+        } else if (media<7) { System.out.print("Recuperação"); 
+        } else { System.out.print("Aprovado!") ;}
     }
 }
