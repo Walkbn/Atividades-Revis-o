3ba99bd0990ec8a2191481ff21f0cf6bbb0c8592
Walkbn <Walk2233bueno1@gmail.com>
2024-06-03 22:01:48 -0300

    Update ContadorImpar.java

--- ContadorImpar.java
+++ ContadorImpar.java
@@ -1,9 +1,13 @@
 public class ContadorImpar {
     public static void main(String[] args) {
+        int cont = 0;
         for (int i = 1; i<100; i++) {
             if (i % 2 == 1) {
             System.out.print(" " + i + "  ||");
+            cont++;
             }
         }
+        System.out.println("");
+        System.out.println("Existem " + cont + " números pares de 1 até 100.");
     }
 }
